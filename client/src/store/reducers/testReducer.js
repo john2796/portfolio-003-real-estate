c725c3c9ef83f9f5df7769469b6ece4789b67617
john2796 <jbmiranda22796@gmail.com>
2019-01-24 09:47:11 -0800

    init

---
+++ client/src/store/reducers/testReducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+  testing: whatever
+};
+export default function reducerName(state = initialState, action) {
+  switch (action.type) {
+    // case 'ACTION_TYPE':
+    //   return code
+    default:
+      return state;
+  }
+}
