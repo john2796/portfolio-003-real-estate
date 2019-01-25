6d187a3e9131f8143709d4f9e576257a0ad03f05
john2796 <jbmiranda22796@gmail.com>
2019-01-25 01:53:04 -0800

    adding Latest listings tabs

--- client/src/components/layout/MainContainer/LatestListing/LatestListing.js
+++ client/src/components/layout/MainContainer/LatestListing/LatestListing.js
@@ -5,7 +5,44 @@ class LatestListing extends Component {
   render() {
     return (
       <React.Fragment>
-        <h2 className="listing-title">Our latest listings</h2>
+        <svg
+          className="uvc-svg-triangle"
+          xmlns="http://www.w3.org/2000/svg"
+          version="1.1"
+          fill="#fff"
+          width="100%"
+          height={60}
+          viewBox="0 0 0.156661 0.1"
+          style={{ height: 60 }}
+        >
+          <polygon points="0.156661,3.93701e-006 0.156661,0.000429134 0.117665,0.05 0.0783307,0.0999961 0.0389961,0.05 -0,0.000429134 -0,3.93701e-006 0.0783307,3.93701e-006 " />
+        </svg>
+        <div className="listing-wrapper">
+          <h2 className="listing-title">Our latest listings</h2>
+          <nav className="listing-nav">
+            <NavLink exact to="/apartments" activeClassName="current-listing">
+              Apartments
+            </NavLink>
+            <NavLink to="/houses" activeClassName="current-listing">
+              Houses
+            </NavLink>
+            <NavLink to="/villas" activeClassName="current-listing">
+              Villas
+            </NavLink>
+            <NavLink to="/retail" activeClassName="current-listing">
+              Retail
+            </NavLink>
+            <NavLink to="/land" activeClassName="current-listing">
+              Land
+            </NavLink>
+          </nav>
+          <Route
+            path="/houses"
+            render={() => {
+              return <div>testing</div>;
+            }}
+          />
+        </div>
       </React.Fragment>
     );
   }
