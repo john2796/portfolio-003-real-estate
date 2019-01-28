444475669e6c6ccd7325b6bfd45786e99f650f3f
john2796 <jbmiranda22796@gmail.com>
2019-01-28 10:53:47 -0800

    working on footer

--- client/src/components/layout/FooterContainer/Footer.js
+++ client/src/components/layout/FooterContainer/Footer.js
@@ -1,11 +1,99 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Footer = () => {
-  return (
-    <div>
-      <p>footer</p>
-    </div>
-  );
-};
+const latestlisting = [
+  {
+    image:
+      "https://losangeles.wpresidence.net/wp-content/uploads/2014/05/house_1920-105x70.jpeg",
+    price: "$ 800",
+    title: "Boutique Space Greenville"
+  },
+  {
+    image:
+      "https://losangeles.wpresidence.net/wp-content/uploads/2014/05/WPEstate1893-105x70.jpg",
+    price: "$ 16,000 / month",
+    title: "Office Space Central Ave"
+  },
+  {
+    image:
+      "https://losangeles.wpresidence.net/wp-content/uploads/2014/05/WPEstateImageAfter0733-105x70.jpg",
+    price: "from $ 55,000",
+    title: "Villa On Washington Ave"
+  }
+];
+class Footer extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div className="footer">
+          <div className="container footer-flexparent">
+            <div className="footer-item">
+              <h3 className="footer-item-title">About</h3>
+              <p>
+                WP RESIDENCE is committed to delivering a high level of
+                expertise, customer service, and attention to detail to the
+                marketing and sales of luxury real estate, and rental
+                properties.{" "}
+                <a href="#">
+                  READ MORE
+                  <i className="fas fa-arrow-right" />
+                </a>{" "}
+                .
+              </p>
+            </div>
+
+            <div className="footer-item">
+              <h3 className="footer-item-title">Contact</h3>
+              <p>
+                <i class="fas fa-building" /> 3755 Commercial St SE Salem,
+                Corner with Sunny Boulevard, 3755 Commercial OR 97302
+              </p>
+              <p>
+                <i class="fa fa-phone" />
+                (305) 555-4446
+              </p>
+              <p>
+                <i class="fa fa-print" />
+                (305) 555-4446
+              </p>
+              <p>
+                <i class="far fa-envelope" />
+                youremails@gmail.com
+              </p>
+              <p>
+                <i class="fab fa-skype" />
+                yourskypeid
+              </p>
+              <p>
+                <i class="fas fa-desktop" />
+                https://yoururl.com
+              </p>
+            </div>
+
+            <div className="footer-item">
+              <h3 className="footer-item-title">Latest Listings</h3>
+              {latestlisting.map(list => {
+                return (
+                  <div className="listings-item">
+                    <img src={list.image} alt={list.title} />
+                    <div className="listing-right-item">
+                      <p>{list.title}</p>
+                      <p> {list.price}</p>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+
+            <div className="footer-item">
+              <h3 className="footer-item-title">Search</h3>
+              <input type="text" placeholder="Type Keyword" />
+              <button>Search</button>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
 
 export default Footer;
