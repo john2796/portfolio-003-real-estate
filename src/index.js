a5da72a9f7c918ad4bff823cde5945d9d56fbfcf
john2796 <jbmiranda22796@gmail.com>
2019-06-12 20:43:06 -0700

    renamed to css folder

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./style/libs/Index.scss";
+import "./css/Index.scss";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
